5a0e880cb502c1e24f9b0122ab60cc33b2b7bd4a
MrMontazer <dr.victor.royalian@gmail.com>
2023-02-24 15:13:02 +0330

    Added Checkbox widget

---
+++ lib/src/widgets/checkbox.dart
@@ -0,0 +1,28 @@
+import 'package:teniyo/src/lib/html.dart';
+import 'package:teniyo/src/property/color.dart';
+import 'package:teniyo/src/property/widget.dart';
+
+class Checkbox extends Widget{
+  final bool value;
+  final Function(bool)? onChanged;
+  final Color? color, borderColor;
+
+  Checkbox({required this.value, this.onChanged, this.color, this.borderColor});
+
+  @override
+  Html build(){
+    return Html(
+      tag: "Teniyo.Checkbox",
+      attributes: {
+        "value": value,
+        "onChanged": onChanged,
+        "sx": {
+          "color" : borderColor?.toHex(),
+          "&.Mui-checked": {
+            "color": color?.toHex(),
+          },
+        }
+      }
+    );
+  }
+}
