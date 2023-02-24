c4f26134d4f522d07793c5f4bf0333a58817abce
MrMontazer <dr.victor.royalian@gmail.com>
2023-02-24 16:51:31 +0330

    Added Radio Widget

---
+++ lib/src/widgets/radio.dart
@@ -0,0 +1,25 @@
+import 'package:teniyo/src/lib/html.dart';
+import 'package:teniyo/src/property/widget.dart';
+import 'package:teniyo/src/lib/not_web.dart' if (dart.library.html) 'package:teniyo/src/lib/is_web.dart';
+
+class Radio extends Widget{
+  final bool value;
+  final Function(bool value)? onChanged;
+
+  Radio({required this.value, this.onChanged});
+
+  @override
+  Html build(){
+    JsFunction onChanged = JsFunction.withThis((self,bool value){
+      this.onChanged?.call(value);
+    });
+
+    return Html(
+      tag: "Teniyo.Radio",
+      attributes: {
+        "value": value,
+        "onChanged": onChanged,
+      },
+    );
+  }
+}
